4401b6c0227a342b82f8e998a8a0e25819e6cbb0
Bintu <bintualkassoum@gmail.com>
2024-05-18 17:23:20 -0400

    Added additional TipTap components to Dashboard page

--- todos/todo-app/src/app/dashboard/page.tsx
+++ todos/todo-app/src/app/dashboard/page.tsx
@@ -1,13 +1,25 @@
 // src/app/dashboard/page.tsx
 
 import React from 'react';
+
 import Editor from '../../components/tasks/TaskEditor'  // Removed the .tsx extension and used a relative path
+import TaskInput from '../../components/tasks/TaskInput'
+import TaskList from '../../components/tasks/TaskList'
+import TaskDelegation from '../../components/tasks/TaskDelegation'
+import TaskScheduling from '../../components/tasks/TaskScheduling'
+import NotificationSettings from '../../components/tasks/NotificationSettings'
 
 export default function DashboardPage() {
   return (
     <div>
       <h1>AI To-Do List</h1>
       <Editor />
+      <h2 className="text-2xl font-bold mb-4">Your Tasks</h2>
+      <TaskInput />
+      <TaskDelegation />
+      <TaskScheduling />
+      <NotificationSettings />
+      <TaskList />
     </div>
   );
 }
